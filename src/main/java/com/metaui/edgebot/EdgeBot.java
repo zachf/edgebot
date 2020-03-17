f8c97a2c1b2a2528cbf9f8db866566221fb504ad
Zach Forsyth <zforsyth@walleyesoftware.com>
2020-03-17 13:16:47 -0400

    Auth

--- src/main/java/com/metaui/edgebot/EdgeBot.java
+++ src/main/java/com/metaui/edgebot/EdgeBot.java
@@ -22,7 +22,7 @@ public class EdgeBot {
         Logger.getLogger("com.metaui").setLevel(Level.FINEST);
 
         AppConfig config = new AppConfig();
-        config.setSingleTeamBotToken("xoxb-1002667100565-1005402728624-BfV3d40HBwqqQTMV5V2aHHwE");
+        config.setSingleTeamBotToken("xoxb-1002667100565-1005402728624-RHBqWX3PWt4yiL3PgK5BgmyW");
         config.setSigningSecret("f0ffc785b30f02e795694f7ceef827b9");
         App app = new App(config);
 
